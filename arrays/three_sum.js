b2d1fb89033762444e6d26adee5cea63b5fa6c6d
Snigdha  Banda <snigdhabanda@Snigdhas-MacBook-Air.local>
2021-12-24 11:16:03 -0500

    method 2 three sum

--- arrays/three_sum.js
+++ arrays/three_sum.js
@@ -1,6 +1,45 @@
 //THREESUM
 //Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
-
+//Method #2
+//time: O(n^2); sorting takes nlogn so overall is n^2 + nlogn which is n^2
+//space: O(n)
+var threeSum = function(nums) {
+    nums.sort()
+    let hash = {}
+    let index = 0
+    let newArray = []
+  
+    var findSum = function(nums, index, newArray){
+        let curr = index + 1
+        let set = new Set()
+        while (curr < nums.length){
+            let comp = -(nums[index] + nums[curr])
+            if (set.has(comp)){
+                newArray.push([nums[curr], nums[index], comp])
+                while(curr + 1 < nums.length && nums[curr] === nums[curr + 1]){
+                    curr += 1
+                }
+            }
+            else{
+                set.add(nums[curr])
+            }
+            curr += 1
+        }
+    }
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] > 0){
+            return newArray; 
+        }
+        if (i=== 0 || nums[i] !== nums[i - 1]){
+            findSum(nums, i, newArray) 
+        }
+        
+    }
+    return newArray; 
+      
+    
+    
+};
 //METHOD #1: pointers 
 // var threeSum = function(nums) {
 //     nums.sort()
