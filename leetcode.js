252895ee5bbdfbb49e94fcd481e84581ac7fc854
Snigdha  Banda <snigdhabanda@Snigdhas-MacBook-Air.local>
2021-12-24 02:27:23 -0500

    spiral matrix

--- leetcode.js
+++ leetcode.js
@@ -1,10 +1,11 @@
 //Arrays (Medium)
 
-// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+//PRODUCT EXCEPT SELF 
+    // Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
-//The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+    //The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 
-//You must write an algorithm that runs in O(n) time and without using the division operation.
+    //You must write an algorithm that runs in O(n) time and without using the division operation.
 
 
 var productExceptSelf = function(nums) {
@@ -86,3 +87,43 @@ var productExceptSelf = function(nums) {
 //     }
     
 //     return newArr; 
+
+//SPIRALMATRIX
+//Given an m x n matrix, return all elements of the matrix in spiral order.
+//O(m*n) time; O(1) space complexity
+var spiralOrder = function(matrix) {
+    let start_row = 0;
+     let start_col = 0;
+     let end_row = matrix.length - 1 
+     let end_col = matrix[0].length - 1
+     let newArr = []
+     while(newArr.length < matrix[0].length * matrix.length){
+         for (let i = start_col; i <= end_col; i ++){
+             newArr.push(matrix[start_row][i])
+         }
+         
+         for(let i = start_row + 1; i <= end_row; i ++){
+             newArr.push(matrix[i][end_col])
+         }
+         
+         if (start_row !== end_row){
+             for (let i = end_col - 1; i >= start_col; i--){
+                 newArr.push(matrix[end_row][i])
+             }
+         }
+        
+         if (start_col !== end_col){
+             for (let i = end_row - 1; i > start_row; i-- ){
+                 newArr.push(matrix[i][start_col])
+             }
+         }
+         start_row += 1
+         end_col -= 1
+         start_col += 1
+         end_row -= 1
+     }
+     return newArr
+     
+     
+     
+ };
