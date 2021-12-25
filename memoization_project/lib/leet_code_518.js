f215619aa79710df70e7becbc4e900173a3a54ba
Snigdha  Banda <snigdhabanda@Snigdhas-MacBook-Air.local>
2021-12-25 15:21:13 -0500

    coin change 2

--- memoization_project/lib/leet_code_518.js
+++ memoization_project/lib/leet_code_518.js
@@ -1,3 +1,18 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+//time: O(n); space: O(amount)
+var change = function(amount, coins) {
+    let memo = Array(amount + 1).fill(0)
+    memo[0] = 1
+    
+    for (let coin of coins){
+        for (let i = coin; i < amount + 1; i++){
+            memo[i] = memo[i] + memo[i - coin]
+        }
+        
+    }
+    
+   return memo[amount]; 
+};
+
